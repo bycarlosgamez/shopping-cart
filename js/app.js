44f67f3222a3a1faff214fbc2754294710b18a6f
Carlos Gámez <hardpx@gmail.com>
2022-11-08 13:24:45 -0600

    added a function to show added items in html

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const cart = document.querySelector("#cart");
 const cartContainer = document.querySelector("#cart-list tbody");
 const emptyCartBtn = document.querySelector("#empty-cart-btn");
 const itemsList = document.querySelector("#items-list");
+let cartItems = [];
 
 // loads the all listeners
 loadEvetListeners();
@@ -29,5 +30,29 @@ function readItemsInfo(item) {
     id: item.querySelector("a").getAttribute("data-id"),
     quantity: 1,
   };
-  console.log(itemInfo);
+
+  //add itemInfo object to cart
+  cartItems = [...cartItems, itemInfo];
+
+  console.log(cartItems);
+  shoppingCart();
+}
+
+// show shopping cart on page
+function shoppingCart() {
+  //clear cart so theres not duplicate elements from prev arrange
+  clearHtmlCart();
+  // add item name to cart
+  cartItems.forEach((item) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `<td>${item.name}</td>`;
+    cartContainer.appendChild(row);
+  });
+}
+
+// delete items from cart
+function clearHtmlCart() {
+  while (cartContainer.firstChild) {
+    cartContainer.removeChild(cartContainer.firstChild);
+  }
 }
